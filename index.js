d9c9398dc25aa3c46b1b8c72c178f92db3a9d917
Richard Melchior <raustin.melchior@gmail.com>
2018-06-01 04:51:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+/**
+ *  add function takes two intger arguments, adds them together and returns the solution
+ **/
 function add(a, b) {
   return a + b;
 }
 
+/**
+* subtract function takes two integer arguments, subtracts them and returns the solution
+**/
 function subtract(a, b) {
   return a - b;
 }
 
+/**
+ *  multiply function takes two integer arguments, multiplys them and returns the solution
+ **/
 function multiply(a, b) {
   return a * b;
 }
